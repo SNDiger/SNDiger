3e62834136eccd5b17c48add06dd98ee2e206aff
SNDiger <sin1123ddd@gmail.com>
2019-12-04 19:50:08 +0900

    Add Clicker Project

---
+++ Clicker/Assets/Script/MainUIController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#pragma warning disable CS0649
+public class MainUIController : MonoBehaviour
+{
+    private static int mUIMoveHash = Animator.StringToHash("Move");
+    [SerializeField] Animator[] mWindowAnims;
+
+    public void MoveWindow(int id)
+    {
+        mWindowAnims[id].SetTrigger(mUIMoveHash);
+    }
+}
---
+++ Clicker/Assets/Script/TitleController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+#pragma warning disable CS0649
+public class TitleController : MonoBehaviour
+{
+    [SerializeField] private Button mStartButton;
+    [SerializeField] private Text mStatusText;
+
+    void Start()
+    {
+        mStartButton.onClick.AddListener(StartMainGame);
+        mStatusText.text = "터치해 시작";
+    }
+
+    public void StartMainGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
